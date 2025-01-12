f1ea5dcfd61a885ab71ffcfad52c904704e625e8
MatiasCecko <matias.cecko@tscng.org>
2025-01-12 15:59:17 +0100

    Add files via upload

---
+++ js/credits.js
@@ -0,0 +1,9 @@
+function credits() {
+    Swal.fire({
+        title: 'Credits',
+        text: 'Matias Čečko 4. RB, 2025',
+        icon: 'warning',
+        confirmButtonText: 'OK',
+        confirmButtonColor: "#000000",
+    });
+}
