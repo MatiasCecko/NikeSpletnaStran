736b470c2e586f0785cc772a4ca49609d74046eb
MatiasCecko <matias.cecko@tscng.org>
2025-01-13 09:57:45 +0100

    Update credits.js

--- js/credits.js
+++ js/credits.js
@@ -1,7 +1,7 @@
 function credits() {
     Swal.fire({
         title: 'Credits',
-        text: 'Matias Čečko 4. RB, 2025',
+        text: 'Remade by: Matias Čečko 4. RB, 2025',
         icon: 'warning',
         confirmButtonText: 'OK',
         confirmButtonColor: "#000000",
